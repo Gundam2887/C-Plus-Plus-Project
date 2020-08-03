7256effe854c1eda7bb4cf60ade8671c5b527373
Gundam2887 <657527502@qq.com>
2020-08-03 16:41:18 +0800

    for

---
+++ practice_3/for.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//for 语法：
+//for(起始赋值;条件;执行语句2){执行语句1}
+
+int main(){
+
+    for(int i = 0;i < 10; i++){
+        cout << i << endl;
+    }
+    system("pause");
+    return 0;
+}

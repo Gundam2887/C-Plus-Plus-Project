1a14ada38510c5225ecf4d2914ec69c7fe1cd71d
Gundam2887 <657527502@qq.com>
2020-08-03 20:33:51 +0800

    goto

---
+++ practice_3/goto.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//goto
+//如果标记的名称存在，执行到goto语句时，会跳转到标记的位置
+
+int main(){
+
+    cout << "1" << endl;
+    cout << "2" << endl;
+    cout << "3" << endl;
+    goto a;
+    cout << "4" << endl;
+    cout << "5" << endl;
+    a:
+    cout << "7" << endl;
+    cout << "8" << endl;
+    cout << "9" << endl;
+
+
+}

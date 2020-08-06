3d0e05b4f7a3eff0e4ccf73d93fd81663fa7840a
Gundam2887 <657527502@qq.com>
2020-08-06 13:13:19 +0800

    空指针

---
+++ practice_6/KongZhiZhen.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//空指针
+//1、空指针用于给指针变量初始化
+//2、空指针是不可以访问的
+//0 ~ 255 之间的内存编号是系统占用的，因此不可以访问
+ 
+int main(){
+    //1、
+    int * p =NULL;
+    //2、
+    // *p= 100;
+    // cout << *p << endl;
+
+
+}
